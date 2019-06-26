e2d755d03cb22e2f37134024f395ed4a394675cd
Raphael Lautenbacher <datingel@raphaels-mbp.localdomain>
2019-06-26 18:34:07 +0200

    working delete + post function for express

--- app.js
+++ app.js
@@ -1,55 +1,88 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const students = require('./db.json').students;
+const zList = require('./db.json');
+const bodyParser = require('body-parser');
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   }),
-// );
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 
 app.get('/', (req, res) => res.send('Welcome to Zertify Api'));
 
-app.get('/zstudents', (req, res) => {
-  return res.send(students);
+//#Students:
+//Show all Students
+app.get('/api/students', (req, res) => {
+  res.send(zList.students);
 });
 
-app.get('/zstudents/:id', (req, res) => {
-  // console.log(req.params.id);
-  const key = req.params.id;
-  const targetStudent = students.filter(student => {
-    return student.id == key;
-  });
-  // console.log(student);
-
-  return res.json({students: targetStudent});
+//get Students by ID
+app.get('/api/students/:id', (req, res) => {
+  const students = zList.students;
+  const student = students.find(target => target.id === parseInt(req.params.id));
+  res.send(student);
 });
 
-app.delete('/zstudents/:id', (req, res) => {
-  const targetStudent = students.filter(student => {
-    return student.id == req.params.id;
-  });
-  const index = students.indexOf(targetStudent[0]);
+//Post new students
+app.post('/api/students/', (req, res) => {
+  const students = zList.students;
+  const lastItem = students[students.length - 1];
+  const lastId = lastItem.id;
+  const student = {
+    id: lastId + 1,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+
+  students.push(student);
+  res.send(student);
+});
+
+//delete students by ID
+app.delete('/api/students/:id', (req, res) => {
+  const students = zList.students;
+  const student = students.find(target => target.id === parseInt(req.params.id));
+  const index = students.indexOf(student);
   students.splice(index, 1);
-  // console.log(jsonData);
-
-  return res.json(students);
+  res.send(student);
 });
 
-//   return (
+//#Courses
+//Show all Courses
+app.get('/api/courses', (req, res) => {
+  res.send(zList.courses);
+});
 
-//     err => {
-//       if (err) {
-//         console.log(err);
-//         res.status(500).send('Error deleting student');
-//       } else {
-//         res.sendStatus(200);
-//       }
-//     }
-//   );
-// })
+//show courses by ID
+app.get('/api/courses/:id', (req, res) => {
+  const courses = zList.courses;
+  const course = courses.find(target => target.id === parseInt(req.params.id));
+  res.send(course);
+});
+
+//Post new Courses
+app.post('/api/courses/', (req, res) => {
+  const courses = zList.courses;
+  const lastItem = courses[courses.length - 1];
+  const lastId = lastItem.id;
+  const course = {
+    id: lastId + 1,
+    name: req.body.name,
+    hours: req.body.hours,
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+//delete courses by id
+app.delete('/api/courses/:id', (req, res) => {
+  const courses = zList.courses;
+  const course = courses.find(target => target.id === parseInt(req.params.id));
+  res.send(course);
+});
 
 app.listen(port, () => console.log(`Zertify api listening on ${port}!`));
