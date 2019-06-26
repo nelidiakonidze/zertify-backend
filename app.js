07afcd3313cd78db2a55c77753083135b66e90dc
Neli <diakonidzeneli@gmail.com>
2019-06-26 15:34:20 +0200

    fixing

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ app.all('/', function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
-app.get('/', function(req, res, next) {
-  // Handle the get for this route
-});
-app.post('/', function(req, res, next) {
-  // Handle the post for this route
-});
+
 app.get('/', (req, res) => res.send('Welcome to Zertify Api'));
 
 app.get('/zstudents', (req, res) => {
